ac8f2881ff13204d2228c2422f18a6f811ab1450
Gregooz <gregoire.decamp@gmail.com>
2016-02-04 10:38:05 +0100

    Modification tables, ajout des FK

--- src/BD.py
+++ src/BD.py
@@ -10,59 +10,73 @@ class BD:
 	##         Connexion à la base de données             ##
 	########################################################
 	def connexion(self):
-		self.cnx = connection.MySQLConnection(user='E146294Q', password='E146294Q', host='infoweb', database='E146294Q')
+		self.cnx = connection.MySQLConnection(user='E146103H', password='E146103H', host='infoweb', database='E146103H')
 		self.cursor = self.cnx.cursor()
 
 	########################################################
-	##         Création de la table installations         ##
+	##         Création de la table installation         ##
 	########################################################
 	def creationTables(self):
 		TABLES = {}
-		TABLES['installations'] = (
-    	"CREATE TABLE `installations` ("
-    	"  `nom_installation` varchar(255) NOT NULL,"
-    	"  `numero_installation` int(20) NOT NULL,"
-    	"  `nom_commune` varchar(255) NOT NULL,"
-    	"  `code_insee` int(10) NOT NULL,"
-    	"  `date_maj` date NOT NULL,"
-    	"  `longitude` float(15) NOT NULL,"
-    	"  `latitude` float(15) NOT NULL,"
-    	"  PRIMARY KEY (`code_insee`)"
+		TABLES['installation'] = (
+    	"CREATE TABLE `installation` ("
+    	"	`id` int(10) NOT NULL,"
+    	"   `nom` varchar(255) NOT NULL,"
+    	"   `adresse` varchar(255) NOT NULL,"
+    	"   `code_postal` int(5) NOT NULL,"
+    	"   `ville` varchar(255) NOT NULL,"
+    	"   `latitude` float(15) NOT NULL,"
+    	"   `longitude` float(15) NOT NULL,"
+    	"   PRIMARY KEY (`id`)"
     	") ")
 
 		########################################################
 		##         Création de la table équipements          ##
 		########################################################
 
-		TABLES['equipements'] = (
-    	"CREATE TABLE `equipements` ("
-    	"  `code_INSEE` int(10) NOT NULL,"
-    	"  `nom_commune` varchar(255) NOT NULL,"
-    	"  `nom_equipement` varchar(255) NOT NULL,"
-	    "  `id_equipement` varchar(255) NOT NULL,"
-	    "  PRIMARY KEY (`code_INSEE`)"
+		TABLES['equipement'] = (
+    	"CREATE TABLE `equipement` ("
+    	"	`id` int(10) NOT NULL,"
+    	"   `nom` varchar(255) NOT NULL,"
+	    "   `id_installation` int(10) NOT NULL,"
+	    "   PRIMARY KEY (`id`),"
+	    "   CONSTRAINT `fk_installation` FOREIGN KEY (`id_installation`)"
+	    "		REFERENCES `installation` (`id`) ON DELETE CASCADE"
 	    ") ")
-
 		########################################################
 		##         Création de la table activités             ##
 		########################################################
 
-		TABLES['activites'] = (
-		    "CREATE TABLE `activites` ("
-		    "  `code_INSEE` int(10) NOT NULL,"
-		    "  `num_fiche_equipement` int(15) NOT NULL,"
-		    "  `activite_libelle` varchar(255) NOT NULL,"
-		    "  `nom_commune` varchar(255) NOT NULL,"
-		    "  PRIMARY KEY (`code_INSEE`)"
+		TABLES['activite'] = (
+		    "CREATE TABLE `activite` ("
+		    "  `id` int(10) NOT NULL,"
+		    "  `nom` varchar(255) NOT NULL,"
+		    "  PRIMARY KEY (`id`)"
 		    ") ")
 
+		########################################################
+		##       Création de la table equipement_activite    ##
+		########################################################		
+		TABLES['equipement_activite'] = (
+			"CREATE TABLE `equipement_activite` ("
+			"  `id_equipement` int(10) NOT NULL,"
+			"  `id_activite` int(10) NOT NULL,"
+			"	PRIMARY KEY (`id_equipement`, `id_activite`),"
+			"	KEY `id_equipement` (`id_equipement`),"
+			"	KEY `id_activite` (`id_activite`),"
+			"	CONSTRAINT `fk_equipement` FOREIGN KEY (`id_equipement`)"
+			"		REFERENCES `equipement` (`id`) ON DELETE CASCADE,"
+			"	CONSTRAINT `fk_activite` FOREIGN KEY (`id_activite`)"
+			"		REFERENCES `activite` (`id`) ON DELETE CASCADE"
+			") ")
 
-		try:
-			self.cursor.execute(TABLES['equipements'])
-			self.cursor.execute(TABLES['installations'])
-			self.cursor.execute(TABLES['activites'])
-		except:
-			print("Les Tables sont déja crée")
+		self.cursor.execute(TABLES['installation'])
+		self.cursor.execute(TABLES['equipement'])
+		self.cursor.execute(TABLES['activite'])
+		self.cursor.execute(TABLES['equipement_activite'])
+		# try:
+		# except:
+		# 	print("Les Tables sont déja crée")
 
 
 	def deconnexion(self):
